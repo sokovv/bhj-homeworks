ea8b3efd639582f49de3ef85eef583d743a7fd8c
Valera Sokolov <komproekt42@gmail.com>
2021-12-14 14:29:48 +0300

    first commit popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,22 @@
+let modulMain = document.getElementById('modal_main')
+let modulSuccess = document.getElementById('modal_success')
+let modulClose = document.getElementsByClassName('modal__close')
+let showSuccess = document.getElementsByClassName('show-success')
+
+//modulMain.classList.add('modal_active');
+modulMain.className = "modal modal_active"
+
+function Success() {
+    modulSuccess.classList.add('modal_active');
+}
+
+function CloseMain() {
+    modulMain.classList.remove('modal_active');
+    modulSuccess.classList.remove('modal_active');
+}
+
+modulClose[0].onclick = CloseMain;
+showSuccess[0].onclick = Success;
+modulClose[2].onclick = CloseMain;
+
+
