d3413e612e180dd861f11fd7c98f99d14cc52e29
Valera Sokolov <komproekt42@gmail.com>
2021-12-30 16:37:13 +0300

    second commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,32 +3,26 @@ let input = document.getElementById("task__input")
 let task = document.getElementsByClassName("task")
 
 input.addEventListener('keydown', e => {
-    if (e.keyCode === 13 && input.textLength > 0) {
-        list[0].innerHTML += `
+    if (e.key === "Enter" && input.value.trim() != "") {
+        list[0].insertAdjacentHTML('beforeEnd', `
         <div class="task">
         <div class="task__title">
-        ${input.value}
+        ${input.value.trim()}
         </div>
         <a href="#" class="task__remove">&times;</a>
-      </div>`
+      </div>`)
             ;
-    }
-    let remove = document.getElementsByClassName("task__remove")
-
-    for (let i = 0; i < remove.length; i++) {
-
-        function getlink(i) {
-            let linkClick = remove[i];
-            return linkClick;
-        }
-        let link = getlink(i);
-
-        function rem() {
-            link.closest('.task').remove()
-        }
-        link.addEventListener('click', rem)
-
+        event.preventDefault()
     }
 })
+
+function rem(e) {
+    if (e.target.className === "task__remove") {
+        e.target.parentElement.remove()
+    }
+    event.preventDefault()
+}
+
+list[0].addEventListener('click', rem);
 
 
