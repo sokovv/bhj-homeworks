dfd9b55af9a49b6bf86471ab1010e56509693701
Valera Sokolov <komproekt42@gmail.com>
2021-12-31 14:23:45 +0300

    four commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,24 +3,26 @@ let input = document.getElementById("task__input")
 let task = document.getElementsByClassName("task")
 let button = document.getElementById('tasks__add');
 
-
-button.addEventListener('click', e => {
-    e.preventDefault()
-})
-
-input.addEventListener('keydown', e => {
-    if (e.key === "Enter" && input.value.trim() != "") {
+function taskadd(e) {
+    if (input.value.trim() != "") {
         list[0].insertAdjacentHTML('beforeEnd', `
-        <div class="task">
-        <div class="task__title">
-        ${input.value.trim()}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-      </div>`)
+    <div class="task">
+    <div class="task__title">
+    ${input.value.trim()}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+  </div>`)
             ;
         input.value = "";
-        e.preventDefault()
+    }
+    e.preventDefault()
+}
 
+button.addEventListener('click', taskadd)
+
+input.addEventListener('keydown', e => {
+    if (e.key === "Enter") {
+        taskadd
     }
 })
 
